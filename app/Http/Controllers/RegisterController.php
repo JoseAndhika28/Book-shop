dbeaeea05803bf597c2f1a42e8450cb79a99ca88
ibnuhami <aufafawwaz12@gmail.com>
2025-03-31 01:34:26 +0700

    penambahan penjelasan

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -21,13 +21,16 @@ class RegisterController extends Controller
     {
         // Untuk debugging juga bisa pake kayak gini. Fungsinya buat ngecek data apa yang dikirim dari depan (Fe) ke belakang (Be)
         Log::info($request->all());
+
+        // Perhatiin Rules yang ada di Validator, itu data request yang biasanya diperlukan dari FE ke BE
         $validator = Validator::make($request->all(), [
             'email' => 'required|email|unique:users|max:255',
-            'name' => 'required|unique:users|max:255', // Sesuai dokumentasi penggunaan unique seperti ini unique:nama_table,nama_kolom
+            'name' => 'required|unique:users|max:255', // Sesuai dokumentasi penggunaan unique seperti ini unique:nama_table,nama_kolom -> Docs https://stackoverflow.com/questions/25663478/laravel-validator-unique-value
             'password' => 'required|min:8'
         ]);
 
         if ($validator->fails()) {
+            // Biasanya juga dikasih Log kayak gini buat nampilin errornya di storage/logs/laravel.log
             Log::info('Validation failed: ' . json_encode($validator->errors()));
             return redirect(route('register'))
                 ->withErrors($validator)
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('create');
+Route::post('/register', [RegisterController::class, 'store'])->name('create'); // Route untuk menyimpan data registrasi namanya itu create. Itu bisa dilihat dari ->name('create') di route
